33b376b0272b714954a7e6dd9418a0e6ff562b8b
reall <reall@MSI>
2024-02-27 23:32:16 -0700

    almost there, just not updating the subject correctly

--- Observer1/Data/ObserverTwo.cs
+++ Observer1/Data/ObserverTwo.cs
@@ -1,14 +1,29 @@
-﻿namespace Observer1.Data;
+﻿using Microsoft.JSInterop;
+using System.Windows.Markup;
+
+
+namespace Observer1.Data;
 
 public class ObserverTwo : IObserver, IDisplayElement
 {
+    private int numHomeworkAssignments;
+    private int daysTilBreak;
+    private int stress;
     public void DisplayStats()
     {
-        throw new NotImplementedException();
+        using StreamWriter writer = new StreamWriter("StressLevelStats.txt", append:true);
+        writer.WriteLine($"Timestamp: {DateTime.Now}\tHomework Assignments Due: {numHomeworkAssignments}\tDays Until Spring Break: {daysTilBreak}\t\tOverall Stress Level: {stress}");
     }
 
     public void Update(int homeworkAssigments, int daysUntilBreak)
     {
-        throw new NotImplementedException();
+        numHomeworkAssignments = homeworkAssigments;
+        daysTilBreak = daysUntilBreak;
+        if (daysTilBreak > 0)
+        {
+            stress = homeworkAssigments / daysTilBreak;
+        }
+       else { stress = 0; }
+       DisplayStats(); //notify the world that our statistics changed
     }
 }
