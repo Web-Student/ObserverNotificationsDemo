94c47a08c8cb60197a4f59f734485600f2f967bc
reall <reall@MSI>
2024-02-28 10:14:16 -0700

    It works!!!

--- Observer1/Data/StatisticsSubject.cs
+++ Observer1/Data/StatisticsSubject.cs
@@ -2,14 +2,27 @@
 public class StatisticsSubject : ISubject
 {
     //These are our own personal statistics to track
-    protected int daysUntilSpringBreak = 3;
-    protected int homeworkAssignmentsDueThisWeek = 100;
-    protected double stressLevel = 0.0; //maybe I should move this to a different specific implementation of our observer, stress level = 1/daysLeft * homework due
+    protected int daysUntilSpringBreak { get; set; }
+    protected int homeworkAssignmentsDueThisWeek { get; set; }
+    //protected double stressLevel = 0.0; //maybe I should move this to a different specific implementation of our observer, stress level = 1/daysLeft * homework due
     List<IObserver> observers;
+    
+    public void SetMeasurements(int days, int homework)
+    {
+        daysUntilSpringBreak = days;
+        homeworkAssignmentsDueThisWeek = homework;
+    }
+
+    public void MeasurementsChanged()
+    {
+        NotifyObservers();
+    }
 
     public StatisticsSubject()
     {
         observers = new List<IObserver>();
+        daysUntilSpringBreak = 7;
+        homeworkAssignmentsDueThisWeek = 50;
     }
 
     //these are the default methods we have to implement
